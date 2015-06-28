4b8ea8e44e52a562c0ff415f0c56d4889e8050f4
sebi <gore.sebyx@yahoo.com>
2015-06-28 18:45:26 +0300

    change from close to keep alive

--- lib/dota_steam/requests/steam_user_request.rb
+++ lib/dota_steam/requests/steam_user_request.rb
@@ -10,11 +10,12 @@ module DotaSteam
         @steam_ids = _steam_ids
       end
 
-      def run
-        req = HTTP.get(URL, params: {key: DotaSteam::ApiKeyProvider.get_key, steamids: @steam_ids.join(',')})
+      def run(http=HTTP)
+        req = http.get(URL, params: {key: DotaSteam::ApiKeyProvider.get_key, steamids: @steam_ids.join(',')})
         @status = req.status.to_i
         @body = req.body
-        true
+
+        http
       end
     end
   end
