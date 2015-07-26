99995723e471132446769daab742915578e39279
sebi <gore.sebyx@yahoo.com>
2015-07-26 11:58:10 +0300

    ggff

--- lib/dota_steam.rb
+++ lib/dota_steam.rb
@@ -1,6 +1,5 @@
-require 'dota_steam/version'
 require 'dota_steam/parsers/match_history_parser'
-require 'dota_steam/parsers/user_parser'
 
 module DotaSteam
+
 end
---
+++ lib/dota_steam/version.rb
@@ -0,0 +1,3 @@
+module DotaSteam
+  VERSION = '0.0.1'
+end
