0928c4200d95111d21967e9cf962997d425cfe87
sebi <gore.sebyx@yahoo.com>
2015-07-27 00:29:35 +0300

    finish profiler

--- lib/dota_steam/profilers/player_profiler.rb
+++ lib/dota_steam/profilers/player_profiler.rb
@@ -1,7 +1,10 @@
 module DotaSteam
   module Profilers
     module PlayerProfiler
-
+      def role_by_hero(hero_id)
+        cache = DotaSteam.configuration.heroes_cache
+        cache.get(hero_id)[:role]
+      end
     end
   end
 end
