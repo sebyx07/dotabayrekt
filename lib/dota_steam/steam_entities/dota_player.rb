f10b5aefabe116e4e377f212ad2057643e96e0c2
sebi <gore.sebyx@yahoo.com>
2015-07-26 12:35:19 +0300

    add playe from full

--- lib/dota_steam/steam_entities/dota_player.rb
+++ lib/dota_steam/steam_entities/dota_player.rb
@@ -1,7 +1,9 @@
 module DotaSteam
   module SteamEntities
     class DotaPlayer
-      attr_accessor :account_id, :player_slot, :hero_id
+      attr_accessor :account_id, :player_slot, :hero_id, :items, :bear_items, :kills, :deaths, :assists, :leaver_status,
+                    :gold, :last_hits, :denies, :gpm, :xpm, :gold_spent, :hero_damage, :tower_damage, :hero_healing,
+                    :level, :lvlups
 
       #Static
       class << self
@@ -13,6 +15,48 @@ module DotaSteam
           player.hero_id = hash['hero_id']
 
           player
+        end
+
+        def new_from_full(hash)
+          player = new_from_history(hash)
+          add_items player, hash
+          player.kills = hash['kills']
+          player.deaths = hash['deaths']
+          player.assists = hash['assists']
+          player.leaver_status = hash['leaver_status']
+          player.gold = hash['gold']
+          player.last_hits = hash['last_hits']
+          player.denies = hash['denies']
+          player.gpm = hash['gold_per_min']
+          player.xpm = hash['xp_per_min']
+          player.gold_spent = hash['gold_spent']
+          player.hero_damage = hash['hero_damage']
+          player.tower_damage = hash['tower_damage']
+          player.hero_healing = hash['hero_healing']
+          player.level = hash['level']
+          add_lvlups player, hash
+          player
+        end
+
+        protected
+        def add_items(player, hash)
+          player.items = []
+          player.items.push(hash['item_0'], hash['item_1'], hash['item_2'], hash['item_3'], hash['item_4'], hash['item_5'])
+
+          if player.hero_id == 80
+            player.bear_items = []
+            root = hash['additional_units'][0]
+            player.bear_items.push(root['item_0'], root['item_1'], root['item_2'], root['item_3'], root['item_4'], root['item_5'])
+          end
+        end
+
+        def add_lvlups(player, hash)
+          player.lvlups = []
+          root = hash['ability_upgrades']
+
+          root.each do |ability|
+            player.lvlups.push ability['time']
+          end
         end
       end
     end
--- spec/steam_entities/dota_player_spec.rb
+++ spec/steam_entities/dota_player_spec.rb
@@ -3,22 +3,46 @@ require 'dota_steam/steam_entities/dota_player'
 require 'json'
 
 describe 'DotaSteam::SteamEntities::DotaPlayer' do
-  let(:player_hash) do
+  let(:player_hash_history) do
     content = File.read('./json_responses/getMatchHistory.json')
     json = JSON.parse(content)
     json['result']['matches'][0]['players'][0]
   end
 
-  let(:player_attributes) do
+  let(:player_hash_full) do
+    content = File.read('./json_responses/getMatchDetails.json')
+    json = JSON.parse(content)
+    json['result']['players'][6]
+  end
+
+  let(:player_attributes_history) do
     [:account_id, :player_slot, :hero_id]
+  end
+
+  let(:player_attributes_full) do
+    player_attributes_history.push(
+      :items, :bear_items, :kills, :deaths, :assists, :leaver_status, :gold, :last_hits, :denies, :gpm, :xpm, :gold_spent,
+      :hero_damage, :tower_damage, :hero_healing, :level, :lvlups
+    )
   end
 
   describe '#self.new_from_history' do
     it 'creates a player' do
-      player = DotaSteam::SteamEntities::DotaPlayer.new_from_history(player_hash)
+      player = DotaSteam::SteamEntities::DotaPlayer.new_from_history(player_hash_history)
       expect(player).not_to be nil
 
-      player_attributes.each do |at|
+      player_attributes_history.each do |at|
+        expect(player.send(at)).not_to be nil
+      end
+    end
+  end
+
+  describe '#self.new_from_full' do
+    it 'creates a player' do
+      player = DotaSteam::SteamEntities::DotaPlayer.new_from_full(player_hash_full)
+      expect(player).not_to be nil
+
+      player_attributes_full.each do |at|
         expect(player.send(at)).not_to be nil
       end
     end
