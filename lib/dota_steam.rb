c41265be2128499bf514317b118f75a180ad4d23
sebi <gore.sebyx@yahoo.com>
2015-07-28 13:47:42 +0300

    fix bug not requiring profile

--- lib/dota_steam.rb
+++ lib/dota_steam.rb
@@ -4,6 +4,7 @@ require 'dota_steam/parsers/match_details_parser'
 require 'dota_steam/rate/role_assessment'
 require 'dota_steam/rate/match_rate'
 require 'dota_steam/cache/base_cache'
+require '../lib/dota_steam/profilers/player_profiler'
 
 module DotaSteam
   include Configurations
