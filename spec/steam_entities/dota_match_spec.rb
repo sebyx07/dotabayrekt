f5b641679564880969119e530cc024a51040afb7
sebi <gore.sebyx@yahoo.com>
2015-08-02 22:43:36 +0300

    add basic logging

--- spec/steam_entities/dota_match_spec.rb
+++ spec/steam_entities/dota_match_spec.rb
@@ -76,4 +76,16 @@ describe 'DotaSteam::SteamEntities::DotaMatch' do
       expect(match.dire.size).to eq 1
     end
   end
+
+  describe '#self.add_players_history' do
+    before do
+      @match = DotaSteam::SteamEntities::DotaMatch.new
+    end
+    context 'hash has no players' do
+      it 'players are [] and warns' do
+        DotaSteam::SteamEntities::DotaMatch.add_players_history(@match, {})
+        expect(@match.players).to be_empty
+      end
+    end
+  end
 end
