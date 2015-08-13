c93347fecc792a0958a6a52bea4d0fc3892e20e2
sebi <gore.sebyx@yahoo.com>
2015-08-13 15:56:48 +0300

    add more tests for parse_number_history

--- spec/parsers/match_history_parser_spec.rb
+++ spec/parsers/match_history_parser_spec.rb
@@ -51,4 +51,31 @@ describe 'DotaSteam::Parsers::MatchHistoryParser' do
       expect(parser.status).to eq :done
     end
   end
+
+
+  describe '#parse_number_history' do
+    let(:parser){ DotaSteam::Parsers::MatchHistoryParser.new(account_id: 1234) }
+
+    before do
+      stub_request(:get, 'http://api.steampowered.com/IDOTA2Match_570/GetMatchHistory/V001/?account_id=1234&key=EE4B46697AAE3B64E5E4334E10E7AB0F').
+          with(headers: { connection: 'Keep-Alive', host: 'api.steampowered.com', user_agent: 'http.rb/0.9.0'}).
+          to_return(:status => 200, :body => File.read('./json_responses/getMatchHistory.json'), :headers => {})
+
+      stub_request(:get, 'http://api.steampowered.com/IDOTA2Match_570/GetMatchHistory/V001/?account_id=1234&key=EE4B46697AAE3B64E5E4334E10E7AB0F&start_at_match_id=1589628135').
+          with(headers: { connection: 'Keep-Alive', host: 'api.steampowered.com', user_agent: 'http.rb/0.9.0'}).
+          to_return(:status => 200, :body => File.read('./json_responses/getMatchHistoryLast.json'), :headers => {})
+    end
+
+    it 'returns 1 matches' do
+      parser.parse_number_history 1
+      expect(parser.result.size).to eq 1
+      expect(parser.status).to eq :done
+    end
+
+    it 'returns 3 matches' do
+      parser.parse_number_history 3
+      expect(parser.result.size).to eq 3
+      expect(parser.status).to eq :done
+    end
+  end
 end
